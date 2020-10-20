9ddcc6e04e607fba9f57daab567bf8d9746320ff
unknown <minhphuson1999@gmail.com>
2020-10-20 20:48:46 +0700

    them brand controller

--- api/controllers/brand.controller.js
+++ api/controllers/brand.controller.js
@@ -1,30 +1,29 @@
 'use strict'
-const category = require('../models/category.model');
-exports.getCategory=(req,res)=>{
-    category.find({},(err,res)=>{
-        if(err){
+const brand = require('../models/brand.model');
+exports.getBrand = (req, res) => {
+    category.find({}, (err, docs) => {
+        if(err) {
             console.log(err);
-        }
+        } 
         res.status(200).json({data:docs});
     })
 }
-
-exports.getAll=async(req,res)=>{
-    if(typeof req.params.page === 'undefined'){
-        res.status(402).json({msg:'Data Invalid'});
+exports.getAll = async (req, res) => {
+    if(typeof req.params.page === 'undefined') {
+        res.status(402).json({msg: 'Data invalid'});
         return;
     }
     let count = null;
-    try{
-        count = await category.count({});
+    try { 
+        count = await brand.count({})
     }
-    catch(err){
+    catch(err) {
         console.log(err);
-        res.status(500).json({msg:err});
+        res.status(500).json({msg: err});
         return;
     }
-    let totalPage = parseInt(((count-1)/9)+1);
-    let {page}=req.params;
+    let totalPage = parseInt(((count - 1) / 9) + 1);
+    let { page } = req.params;
     if ((parseInt(page) < 1) || (parseInt(page) > totalPage)) {
         res.status(200).json({ data: [], msg: 'Invalid page', totalPage });
         return;
@@ -41,32 +40,31 @@ exports.getAll=async(req,res)=>{
         res.status(200).json({ data: docs, totalPage });
     })
 }
-
 exports.getNameByID = async (req, res) => {
     if(req.params.id === 'undefined') {
         res.status(422).json({ msg: 'Invalid data' });
         return;
     }
-    let result;
+    let result
     try {
-        result = await category.findById(req.params.id);
+        result = await brand.findById(req.params.id);
     }
     catch(err) {
-        console.log(err);
-        res.status(500).json({msg: err});
+        console.log(err)
+        res.status(500).json({msg: err})
         return;
     }
     if(result === null){
-        res.status(404).json({msg: "not found"});
+        res.status(404).json({msg: "not found"})
         return;
     }
-    res.status(200).json({name: result.name});
-}  
+    res.status(200).json({name: result.name})
+}
 
 exports.getIDBySearchText = async (searchText) => {
     let arr = [];
     try {
-        arr = await category.find({name: new RegExp(searchText, "i")},{name: 0});
+        arr = await brand.find({name: new RegExp(searchText, "i")},{name: 0});
     }
     catch (err) {
         res.status(500).json({ msg: err });
